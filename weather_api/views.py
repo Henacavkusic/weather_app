f25950c22bd7e0c2a9640aafbd6fcaa5094f120d
Hena Cavkusic <henacav@gmail.com>
2023-05-14 14:22:10 +0200

    added caching using redis

--- weather_api/views.py
+++ weather_api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.cache import cache
 from django.http import JsonResponse
 from datetime import date, datetime
 import logging
@@ -8,59 +9,80 @@ from weather_api.models import WeatherData
 
 api_logger = logging.getLogger(__name__)
 
+
 def current_weather(request):
     location = request.GET['location']
-    lat, lon, err = get_lat_lon(location)
-    if not err:
-        url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={env.OWM_API_KEY}&units=metric"
-        api_logger.info(f"Getting current weather data for {location}")
-        response = requests.get(url)
-        if response.status_code != 200:
-            api_logger.error("error: ", response.json())
-            return JsonResponse({"error": f"{response.json()}"}, status=response.status_code)
+    wd = cache.get(f"{location}:current", [])
+    if not wd:
+        lat, lon, err = get_lat_lon(location)
+        if not err:
+            url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={env.OWM_API_KEY}&units=metric"
+            api_logger.info(f"Getting current weather data for {location}")
+            response = requests.get(url)
+            if response.status_code != 200:
+                api_logger.error("error: ", response.json())
+                return JsonResponse({"error": f"{response.json()}"}, status=response.status_code)
 
-        data = response.json()
-        api_logger.info(f"Weather data retrieved successfully")
-        return JsonResponse({location: WeatherData(data['current']).to_dict()})
-    return err
+            data = response.json()
+            wd = WeatherData(data['current']).to_dict()
+            cache.set(key=f"{location}:current", value=wd, timeout=600)
+            api_logger.info(f"Weather data retrieved successfully")
+            return JsonResponse({location: wd})
+        return err
+    else:
+        api_logger.info(f"Weather data retrieved from cache successfully")
+        return JsonResponse({location: wd})
 
 
 def forecast_weather(request):
     location = request.GET['location']
-    lat, lon, err = get_lat_lon(location)
-    if not err:
-        url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={env.OWM_API_KEY}&units=metric"
-        api_logger.info(f"Getting forecast weather data for {location}")
-        response = requests.get(url)
-        if response.status_code != 200:
-            api_logger.error("error: ", response.json())
-            return JsonResponse({"error": f"{response.json()}"}, status=response.status_code)
+    wd = cache.get(f"{location}:forecast", [])
+    if not wd:
+        lat, lon, err = get_lat_lon(location)
+        if not err:
+            url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={env.OWM_API_KEY}&units=metric"
+            api_logger.info(f"Getting forecast weather data for {location}")
+            response = requests.get(url)
+            if response.status_code != 200:
+                api_logger.error("error: ", response.json())
+                return JsonResponse({"error": f"{response.json()}"}, status=response.status_code)
 
-        data = response.json()
-        data_to_return = []
-        for day in data['daily']:
-            data_to_return.append(WeatherData(day).to_dict())
-        api_logger.info(f"Weather data retrieved successfully")
-        return JsonResponse({location: data_to_return})
-    return err
+            data = response.json()
+            wd = []
+            for day in data['daily']:
+                wd.append(WeatherData(day).to_dict())
+            cache.set(key=f"{location}:forecast", value=wd, timeout=600)
+            api_logger.info(f"Weather data retrieved successfully")
+            return JsonResponse({location: wd})
+        return err
+    else:
+        api_logger.info(f"Weather data retrieved from cache successfully")
+        return JsonResponse({location: wd})
 
 
 def history_weather(request):
     location = request.GET['location']
     dt = int(datetime.fromisoformat(request.GET['date']).timestamp())
-    lat, lon, err = get_lat_lon(location)
-    if not err:
-        url = f"https://api.openweathermap.org/data/3.0/onecall/timemachine?lat={lat}&lon={lon}&dt={dt}&appid={env.OWM_API_KEY}&units=metric"
-        api_logger.info(f"Getting weather data for {location} for {request.GET['date']}")
-        response = requests.get(url)
-        if response.status_code != 200:
-            api_logger.error("error: ", response.json())
-            return JsonResponse({"error": f"{response.json()}"}, status=response.status_code)
+    wd = cache.get(f"{location}:history", [])
+    if not wd:
+        lat, lon, err = get_lat_lon(location)
+        if not err:
+            url = f"https://api.openweathermap.org/data/3.0/onecall/timemachine?lat={lat}&lon={lon}&dt={dt}&appid={env.OWM_API_KEY}&units=metric"
+            api_logger.info(f"Getting weather data for {location} for {request.GET['date']}")
+            response = requests.get(url)
+            if response.status_code != 200:
+                api_logger.error("error: ", response.json())
+                return JsonResponse({"error": f"{response.json()}"}, status=response.status_code)
 
-        data = response.json()
-        api_logger.info(f"Weather data retrieved successfully")
-        return JsonResponse({location: WeatherData(data['data'][0]).to_dict()})
-    return err
+            data = response.json()
+            wd = WeatherData(data['data'][0]).to_dict()
+            cache.set(key=f"{location}:history", value=wd, timeout=600)
+            api_logger.info(f"Weather data retrieved successfully")
+            return JsonResponse({location: wd})
+        return err
+    else:
+        api_logger.info(f"Weather data retrieved from cache successfully")
+        return JsonResponse({location: wd})
 
 
 def get_lat_lon(location):
