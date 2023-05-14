6c6a53fb5fb7a002ad37536fa430f9b751ab18c1
Hena Cavkusic <henacav@gmail.com>
2023-05-14 14:21:35 +0200

    changed .env to env.py

--- weather_api/views.py
+++ weather_api/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+import requests
+from weather_app import .env
 
 def current_weather(request, location):
-    pass
+    api_key = "YOUR_API_KEY" # replace with your own API key
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}&units=metric"
+
+    response = requests.get(url)
+    if response.status_code != 200:
+        return JsonResponse({"error": "Invalid location input"}, status=400)
+
+    data = response.json()
+    return JsonResponse(data)
 
 
 def forecast_weather(request, location):
