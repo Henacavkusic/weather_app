72ae39763436b7f197baad09b99b77101a5cc4ff
Hena Cavkusic <henacav@gmail.com>
2023-05-14 14:22:10 +0200

    added logging

--- weather_api/views.py
+++ weather_api/views.py
@@ -1,21 +1,26 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from datetime import date, datetime
+import logging
 import requests
 import env
 from weather_api.models import WeatherData
 
+api_logger = logging.getLogger(__name__)
 
 def current_weather(request):
     location = request.GET['location']
     lat, lon, err = get_lat_lon(location)
     if not err:
         url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={env.OWM_API_KEY}&units=metric"
+        api_logger.info(f"Getting current weather data for {location}")
         response = requests.get(url)
         if response.status_code != 200:
+            api_logger.error("error: ", response.json())
             return JsonResponse({"error": f"{response.json()}"}, status=response.status_code)
 
         data = response.json()
+        api_logger.info(f"Weather data retrieved successfully")
         return JsonResponse({location: WeatherData(data['current']).to_dict()})
     return err
 
@@ -25,14 +30,17 @@ def forecast_weather(request):
     lat, lon, err = get_lat_lon(location)
     if not err:
         url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={env.OWM_API_KEY}&units=metric"
+        api_logger.info(f"Getting forecast weather data for {location}")
         response = requests.get(url)
         if response.status_code != 200:
+            api_logger.error("error: ", response.json())
             return JsonResponse({"error": f"{response.json()}"}, status=response.status_code)
 
         data = response.json()
         data_to_return = []
         for day in data['daily']:
             data_to_return.append(WeatherData(day).to_dict())
+        api_logger.info(f"Weather data retrieved successfully")
         return JsonResponse({location: data_to_return})
     return err
 
@@ -43,19 +51,24 @@ def history_weather(request):
     lat, lon, err = get_lat_lon(location)
     if not err:
         url = f"https://api.openweathermap.org/data/3.0/onecall/timemachine?lat={lat}&lon={lon}&dt={dt}&appid={env.OWM_API_KEY}&units=metric"
+        api_logger.info(f"Getting weather data for {location} for {request.GET['date']}")
         response = requests.get(url)
         if response.status_code != 200:
+            api_logger.error("error: ", response.json())
             return JsonResponse({"error": f"{response.json()}"}, status=response.status_code)
 
         data = response.json()
+        api_logger.info(f"Weather data retrieved successfully")
         return JsonResponse({location: WeatherData(data['data'][0]).to_dict()})
     return err
 
 
 def get_lat_lon(location):
     url = f"https://api.openweathermap.org/geo/1.0/direct?q={location}&appid={env.OWM_API_KEY}"
+    api_logger.info(f"Getting latitude and longitude coordinates for {location}")
     response = requests.get(url)
     if response.status_code != 200:
+        api_logger.error("error: ", response.json())
         return 0, 0, JsonResponse({"error": f"{response.json()}"}, status=response.status_code)
 
     data = response.json()
@@ -63,6 +76,3 @@ def get_lat_lon(location):
     lon = data[0]['lon']
     return lat, lon, ''
 
-
-def make_response(data):
-    return
--- weather_app/settings.py
+++ weather_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import logging
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,3 +117,31 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+        'file': {
+            'class': 'logging.FileHandler',
+            'filename': 'logs/api.log',
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['file'],
+            'level': 'INFO',
+        },
+    },
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+    },
+}
+
